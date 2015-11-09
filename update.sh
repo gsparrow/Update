54e6e80d63aacc45f7e03506d7f5273a4329e67e
sparrowg <sparrowg@iusb.edu>
2015-11-09 07:03:20 -0500

    adds in install commands for system76 drivers
    also adds in wavemon, which is a wifi network monitoring tool.

--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+sudo apt-add-repository ppa:system76-dev/stable
+sudo apt-get update
+sudo apt-get install system76-driver
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install vim
 sudo apt-get install emacs
 sudo apt-get install htop
+sudo apt-get install wavemon
 sudo apt-get install lm-sensors
 sudo apt-get install texlive-full
 sudo apt-get install python-pip
