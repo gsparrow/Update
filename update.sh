bd16ea8e192ef492595774a2e90d58ade3511caa
sparrowg <sparrowg@iusb.edu>
2015-11-09 17:43:33 -0500

    ditch the Y/n prompt and assume yes

--- update.sh
+++ update.sh
@@ -2,19 +2,19 @@
 
 sudo apt-add-repository ppa:system76-dev/stable
 sudo apt-get update
-sudo apt-get install system76-driver
+sudo apt-get install --assume-yes system76-driver
 sudo apt-get update
 sudo apt-get dist-upgrade
-sudo apt-get install vim
-sudo apt-get install emacs
-sudo apt-get install sshfs
-sudo apt-get install sshuttle
-sudo apt-get install ispell
-sudo apt-get install htop
-sudo apt-get install wavemon
-sudo apt-get install lm-sensors
-sudo apt-get install texlive-full
-sudo apt-get install python-pip
-sudo apt-get install python-pygame
+sudo apt-get install --assume-yes vim
+sudo apt-get install --assume-yes emacs
+sudo apt-get install --assume-yes sshfs
+sudo apt-get install --assume-yes sshuttle
+sudo apt-get install --assume-yes ispell
+sudo apt-get install --assume-yes htop
+sudo apt-get install --assume-yes wavemon
+sudo apt-get install --assume-yes lm-sensors
+sudo apt-get install --assume-yes texlive-full
+sudo apt-get install --assume-yes python-pip
+sudo apt-get install --assume-yes python-pygame
 pip install -I pillow
 pip install numpy
