3a9a599ddd011334ce45226676478e2314e69b2f
sparrowg <sparrowg@iusb.edu>
2015-11-09 12:04:13 -0500

    adds ssh mounts and spell check

--- update.sh
+++ update.sh
@@ -7,6 +7,8 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install vim
 sudo apt-get install emacs
+sudo apt-get install sshfs
+sudo apt-get install ispell
 sudo apt-get install htop
 sudo apt-get install wavemon
 sudo apt-get install lm-sensors
