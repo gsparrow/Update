50f7edb92be1ef43d2810c62ef6cc798fd326cd5
sparrowg <sparrowg@iusb.edu>
2015-11-09 06:59:43 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt-get install vim
 sudo apt-get install emacs
+sudo apt-get install htop
+sudo apt-get install lm-sensors
 sudo apt-get install texlive-full
 sudo apt-get install python-pip
 sudo apt-get install python-pygame
